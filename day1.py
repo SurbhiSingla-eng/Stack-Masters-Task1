5e2a23199b223b91cde03e317ed0c8e1ecb54b55
Surbhi Singla <singlasurbhi104@gmail.com>
2026-06-01 23:54:01 +0530

    example code snippets

---
+++ day1.py
@@ -0,0 +1,20 @@
+#student a
+for i in range(10):
+    if i % 2 == 0:
+        print(i)
+
+  # student b
+  for num in range(10):
+    if num % 2 == 0:
+        print(num)
+
+    #student c (slightly diff)
+    for i in range(10):
+    if i % 2 == 0:
+        print(i)
+    print("Done")
+
+# student d (highly diff)
+while True:
+    value = input()
+    print(value)
